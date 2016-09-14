40ca898dfcf33b9b4ab76b4b18ee95a8d2b45eb2
Hugo Peek <hugo@fractal-farming.com>
2016-09-14 11:52:55 +0800

    Gitify extract: latest PL edits

--- _data/snippets/sliderLoadAssets.php
+++ _data/snippets/sliderLoadAssets.php
@@ -2,7 +2,9 @@ id: 75
 name: sliderLoadAssets
 description: 'Load CSS and JS dependencies for Slick slider through CDN.'
 category: f_presentation
+snippet: "/**\n * sliderLoadAssets\n *\n * Loads dependencies for the Slick carousel (http://kenwheeler.github.io/slick/).\n */\n\n$assetsPath = $modx->getOption('patternlab.custom_js_path');\n\n// Head\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css');\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css');\n\n// Footer\n$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');\n$modx->regClientScript('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js');\n$modx->regClientScript($assetsPath . '/slider.js');"
 properties: 'a:0:{}'
+content: "/**\n * sliderLoadAssets\n *\n * Loads dependencies for the Slick carousel (http://kenwheeler.github.io/slick/).\n */\n\n$assetsPath = $modx->getOption('patternlab.custom_js_path');\n\n// Head\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css');\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css');\n\n// Footer\n$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');\n$modx->regClientScript('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js');\n$modx->regClientScript($assetsPath . '/slider.js');"
 
 -----
 
@@ -12,13 +14,17 @@ properties: 'a:0:{}'
  * Loads dependencies for the Slick carousel (http://kenwheeler.github.io/slick/).
  */
 
-$assetsPath = $modx->getOption('patternlab.custom_js_path');
+$assetsPathCSS = $modx->getOption('patternlab.custom_css_path');
+$assetsPathJS = $modx->getOption('patternlab.custom_js_path');
+$assetsPathVendor = $modx->getOption('patternlab.custom_vendor_path');
 
 // Head
-$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css');
-$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css');
+$modx->regClientCSS($assetsPathVendor . '/slick-carousel/slick.css');
+$modx->regClientCSS($assetsPathVendor . '/slick-carousel/slick-theme.css');
+$modx->regClientCSS($assetsPathVendor . '/slick-lightbox/slick-lightbox.css');
 
 // Footer
-$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');
-$modx->regClientScript('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js');
-$modx->regClientScript($assetsPath . '/slider.js');
+//$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');
+$modx->regClientScript($assetsPathVendor . '/slick-carousel/slick.min.js');
+$modx->regClientScript($assetsPathVendor . '/slick-lightbox/slick-lightbox.min.js');
+$modx->regClientScript($assetsPathJS . '/slider.js');
--- _data/snippets/statusGridLoadAssets.php
+++ _data/snippets/statusGridLoadAssets.php
@@ -2,7 +2,9 @@ id: 88
 name: statusGridLoadAssets
 description: 'Load JS dependencies for status grid.'
 category: f_presentation
+snippet: "/**\n * statusGridLoadAssets\n *\n */\n\n$assetsPath = $modx->getOption('patternlab.custom_js_path');\n\n// Footer\n$modx->regClientScript($assetsPath . '/tablesort.min.js');"
 properties: 'a:0:{}'
+content: "/**\n * statusGridLoadAssets\n *\n */\n\n$assetsPath = $modx->getOption('patternlab.custom_js_path');\n\n// Footer\n$modx->regClientScript($assetsPath . '/tablesort.min.js');"
 
 -----
 
@@ -11,7 +13,7 @@ properties: 'a:0:{}'
  *
  */
 
-$assetsPath = $modx->getOption('patternlab.custom_js_path');
+$assetsPathJS = $modx->getOption('patternlab.custom_js_path');
 
 // Footer
-$modx->regClientScript($assetsPath . '/tablesort.min.js');
+$modx->regClientScript($assetsPathJS . '/tablesort.js');
