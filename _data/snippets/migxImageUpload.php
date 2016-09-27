ef8e2086092116e8332d2b3128c1f29e152d3c37
Hugo Peek <hugo@fractal-farming.com>
2016-09-28 00:51:20 +0800

    Exclude elements of extra's from Gitify
    And for clarity, also add names to some data filenames with just an ID

--- _data/snippets/migxImageUpload.php
+++
@@ -1,8 +0,0 @@
-id: 21
-name: migxImageUpload
-category: MIGX
-properties: null
-
------
-
-return include $modx->getOption('core_path').'components/migx/model/imageupload/imageupload.php';
--- _data/snippets/sliderLoadAssets.php
+++ _data/snippets/sliderLoadAssets.php
@@ -2,9 +2,7 @@ id: 75
 name: sliderLoadAssets
 description: 'Load CSS and JS dependencies for Slick slider through CDN.'
 category: f_presentation
-snippet: "/**\n * sliderLoadAssets\n *\n * Loads dependencies for the Slick carousel (http://kenwheeler.github.io/slick/).\n */\n\n$assetsPath = $modx->getOption('patternlab.custom_js_path');\n\n// Head\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css');\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css');\n\n// Footer\n$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');\n$modx->regClientScript('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js');\n$modx->regClientScript($assetsPath . '/slider.js');"
 properties: 'a:0:{}'
-content: "/**\n * sliderLoadAssets\n *\n * Loads dependencies for the Slick carousel (http://kenwheeler.github.io/slick/).\n */\n\n$assetsPath = $modx->getOption('patternlab.custom_js_path');\n\n// Head\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css');\n$modx->regClientCSS('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css');\n\n// Footer\n$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');\n$modx->regClientScript('//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js');\n$modx->regClientScript($assetsPath . '/slider.js');"
 
 -----
 
