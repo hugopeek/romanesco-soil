ea64c44d3b6ed3b82217a8d5fd9899dbc1ca27c7
Hugo Peek <hugo@fractal-farming.com>
2016-10-13 14:54:48 +0800

    Minor corrections in extract

--- _data/snippets/setUserPlaceholders.php
+++ _data/snippets/setUserPlaceholders.php
@@ -2,9 +2,7 @@ id: 83
 name: setUserPlaceholders
 description: 'Make any extended fields that are attached to a MODX user available as placeholder.'
 category: f_framework
-snippet: "/* setUserPlaceholders snippet */\n$userId = $modx->getOption('userId', $scriptProperties, '');\n//$pageId = $modx->getOption('id', $scriptProperties, '');\n\n// Get a specific user\n$usr = $modx->getObject('modUser', $userId);\n\n// Get extended fields of this user\n$profile = $usr->getOne('Profile');\nif ($profile) {\n    $extended = $profile->get('extended');\n    $modx->toPlaceholders($extended, '');\n} else {\n    $modx->log(modX::LOG_LEVEL_ERROR, 'Could not find profile for user: ' . $usr->get('username'));\n}"
 properties: 'a:0:{}'
-content: "/* setUserPlaceholders snippet */\n$userId = $modx->getOption('userId', $scriptProperties, '');\n//$pageId = $modx->getOption('id', $scriptProperties, '');\n\n// Get a specific user\n$usr = $modx->getObject('modUser', $userId);\n\n// Get extended fields of this user\n$profile = $usr->getOne('Profile');\nif ($profile) {\n    $extended = $profile->get('extended');\n    $modx->toPlaceholders($extended, '');\n} else {\n    $modx->log(modX::LOG_LEVEL_ERROR, 'Could not find profile for user: ' . $usr->get('username'));\n}"
 
 -----
 
