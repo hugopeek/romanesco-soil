7b60b2f1adca34c845be2e19eb57dfafd50f5c7a
Hugo Peek <hugo@fractal-farming.com>
2016-10-17 11:40:14 +0800

    Change lexicon prefix in chunks, snippets and templates

--- _data/snippets/fbLoadAssets.php
+++ _data/snippets/fbLoadAssets.php
@@ -5,7 +5,8 @@ properties: 'a:0:{}'
 
 -----
 
-$assetsPath = 'assets/components/patternlab/';
+//$assetsPath = 'assets/components/patternlab/'; // @todo: turn this into the Romanesco SUI themes' dist folder path
+$assetsPath = $this->modx->getOption('romanesco-patterns.custom-js-path');
 
 // Load JS in footer
 $modx->regClientScript($assetsPath.'js/formblocks.js');
