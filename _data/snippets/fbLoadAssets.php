bdba91aa68cb38d7df9c461311925c7fb8dbdfaf
Hugo Peek <hugo@fractal-farming.com>
2016-11-08 23:13:27 +0800

    Minor fixes and tweaks
    - Fix broken path in fbLoadAssets snippet
    - Change node name to menutitle in resource tree
    - Only show active users in person_id TV

--- _data/snippets/fbLoadAssets.php
+++ _data/snippets/fbLoadAssets.php
@@ -5,8 +5,7 @@ properties: 'a:0:{}'
 
 -----
 
-//$assetsPath = 'assets/components/patternlab/'; // @todo: turn this into the Romanesco SUI themes' dist folder path
-$assetsPath = $this->modx->getOption('romanesco-patterns.custom-js-path');
+$assetsPath = $modx->getOption('romanesco.custom_js_path');
 
 // Load JS in footer
 $modx->regClientScript($assetsPath.'js/formblocks.js');
