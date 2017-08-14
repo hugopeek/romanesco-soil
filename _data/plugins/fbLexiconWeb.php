f473afac8776a804d9389aa8abaedda471f3eec4
Hugo Peek <hugo@fractal-farming.com>
2017-08-14 18:06:20 +0800

    Move extracted data to separate repository

--- _data/plugins/fbLexiconWeb.php
+++
@@ -1,8 +0,0 @@
-id: 18
-name: fbLexiconWeb
-category: c_formblocks
-properties: 'a:0:{}'
-
------
-
-$modx->lexicon->load('romanescobackyard:formblocks');
--- _data/plugins/plLexiconWeb.php
+++
@@ -1,8 +0,0 @@
-id: 20
-name: plLexiconWeb
-category: c_global
-properties: 'a:0:{}'
-
------
-
-$modx->lexicon->load('romanescobackyard:default');
