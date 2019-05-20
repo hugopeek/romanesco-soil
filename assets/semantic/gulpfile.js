ff86b1adfa6ac6eeb346a393655c30b23684007b
Hugo Peek <hugo@fractal-farming.com>
2019-05-20 19:35:39 +0800

    Copy dependencies from node_modules to vendors folder with Gulp

--- assets/semantic/gulpfile.js
+++ assets/semantic/gulpfile.js
@@ -78,12 +78,15 @@ if(config.rtl) {
      Custom
 ---------------*/
 
-// Copy assets to project folder.
+// Copy SUI assets to project folder and NPM assets to vendor folder.
 // The root files are excluded from Romanesco to avoid merge conflicts.
 gulp.task('copy', function () {
-  gulp.src('./dist/semantic.*')
-      .pipe(gulp.dest('./dist/project'))
-  ;
+  gulp.src('./dist/semantic.*').pipe(gulp.dest('./dist/project'));
+  gulp.src('../../node_modules/jquery/dist/jquery.min.js').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/jquery'));
+  gulp.src('../../node_modules/slick-carousel/slick/fonts/*').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel/fonts'));
+  gulp.src('../../node_modules/slick-carousel/slick/*.css').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
+  gulp.src('../../node_modules/slick-carousel/slick/*.min.js').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
+  gulp.src('../../node_modules/arrive/minified/*.min.js').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/arrive'));
 });
 
 // Isolate SUI classes for use inside CB chunk previews
