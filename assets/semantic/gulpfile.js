b00c35013daf32beeae31831f9209617e3adc78d
Hugo Peek <hugo@fractal-farming.com>
2019-05-21 22:34:06 +0800

    Tweaks to gulp copy command
    - Add command for copying vendor files from node_modules
    - Use callback function 'done()' to prevent errors in Gulp 4

--- assets/semantic/gulpfile.js
+++ assets/semantic/gulpfile.js
@@ -38,23 +38,35 @@ if (config.rtl) {
      Custom
 ---------------*/
 
-require('gulp-css-wrap')(gulp);
+var cssWrap = require('../../node_modules/gulp-css-wrap');
 
-// Copy SUI assets to project folder and NPM assets to vendor folder.
+// Copy SUI assets to project folder.
 // The root files are excluded from Romanesco to avoid merge conflicts.
-gulp.task('copy', function () {
-  gulp.src('./dist/semantic.*').pipe(gulp.dest('./dist/project'));
-  gulp.src('../../node_modules/jquery/dist/jquery.min.js').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/jquery'));
-  gulp.src('../../node_modules/slick-carousel/slick/fonts/*').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel/fonts'));
-  gulp.src('../../node_modules/slick-carousel/slick/*.css').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
-  gulp.src('../../node_modules/slick-carousel/slick/*.min.js').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
-  gulp.src('../../node_modules/arrive/minified/*.min.js').pipe(gulp.dest('./src/themes/romanesco/assets/vendor/arrive'));
+gulp.task('copy-assets', function (done) {
+  gulp.src('./dist/semantic.*')
+      .pipe(gulp.dest('./dist/project'));
+  done();
+});
+
+// Copy NPM assets to vendor folder.
+gulp.task('copy-vendor', function (done) {
+  gulp.src('../../node_modules/jquery/dist/jquery.min.js')
+      .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/jquery'));
+  gulp.src('../../node_modules/slick-carousel/slick/fonts/*')
+      .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel/fonts'));
+  gulp.src('../../node_modules/slick-carousel/slick/*.css')
+      .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
+  gulp.src('../../node_modules/slick-carousel/slick/*.min.js')
+      .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
+  gulp.src('../../node_modules/arrive/minified/*.min.js')
+      .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/arrive'));
+  done();
 });
 
 // Isolate SUI classes for use inside CB chunk previews
-gulp.task('css-wrap', function() {
+gulp.task('css-wrap', function (done) {
   gulp.src('dist/semantic.css')
       .pipe(cssWrap({selector:'.chunkOutput'}))
-      .pipe(gulp.dest('/home/hugo/Localhost/packages/romanesco-backyard/assets/components/romanescobackyard/css'))
-  ;
+      .pipe(gulp.dest('/home/hugo/Localhost/packages/romanesco-backyard/assets/components/romanescobackyard/css'));
+  done();
 });
