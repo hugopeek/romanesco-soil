9a60b6d8e7a1ce49ad9d9f83212ad8c6cc355a3f
Hugo Peek <hugo@fractal-farming.com>
2020-01-10 17:22:06 +0800

    Improve FUI build process
    - Combine copy-assets and copy-vendor into 1 copy task
    - Build custom CSS/JS elements
    - Concatenate slider assets
    - Generate minified version of all JS files
    - Generate and inline critical-path CSS (WIP)

--- assets/semantic/gulpfile.js
+++ assets/semantic/gulpfile.js
@@ -38,18 +38,11 @@ if (config.rtl) {
      Custom
 ---------------*/
 
-var cssWrap = require('../../node_modules/gulp-css-wrap');
-
-// Copy SUI assets to project folder.
+// Copy SUI assets to project folder and NPM assets to vendor folder.
 // The root files are excluded from Romanesco to avoid merge conflicts.
-gulp.task('copy-assets', function (done) {
+gulp.task('copy', function (done) {
   gulp.src('./dist/semantic.*')
       .pipe(gulp.dest('./dist/project'));
-  done();
-});
-
-// Copy NPM assets to vendor folder.
-gulp.task('copy-vendor', function (done) {
   gulp.src('../../node_modules/jquery/dist/jquery.min.js')
       .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/jquery'));
   gulp.src('../../node_modules/slick-carousel/slick/fonts/*')
@@ -60,13 +53,67 @@ gulp.task('copy-vendor', function (done) {
       .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/slick-carousel'));
   gulp.src('../../node_modules/arrive/minified/*.min.js')
       .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/arrive'));
+  gulp.src('../../node_modules/vanilla-lazyload/dist/*.min.js')
+      .pipe(gulp.dest('./src/themes/romanesco/assets/vendor/vanilla-lazyload'));
+  done();
+});
+
+// Build custom CSS/JS elements
+gulp.task('build-custom', function (done) {
+  const less = require('gulp-less');
+  const autoprefixer = require('gulp-autoprefixer');
+  const concat = require('gulp-concat-css');
+  const minify = require('gulp-clean-css');
+  const uglify = require('gulp-uglify');
+  const rename = require('gulp-rename');
+  const tasks = require('./tasks/config/tasks');
+
+  gulp.src('./src/themes/romanesco/assets/css/slider.less')
+      .pipe(less())
+      .pipe(autoprefixer(tasks.settings.prefix))
+      .pipe(concat('slider.css'))
+      .pipe(minify())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./src/themes/romanesco/assets/css/'));
+  gulp.src(['./src/themes/romanesco/assets/js/*.js','!./src/themes/romanesco/assets/js/*.min.js'])
+      .pipe(uglify(tasks.settings.uglify))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./src/themes/romanesco/assets/js/'));
+
   done();
 });
 
 // Isolate SUI classes for use inside CB chunk previews
 gulp.task('css-wrap', function (done) {
+  const cssWrap = require('../../node_modules/gulp-css-wrap');
+
   gulp.src('dist/semantic.css')
       .pipe(cssWrap({selector:'.chunkOutput'}))
       .pipe(gulp.dest('/home/hugo/Localhost/packages/romanesco-backyard/assets/components/romanescobackyard/css'));
+  done();
+});
+
+// Generate & inline critical-path CSS
+gulp.task('critical', function (done) {
+  const critical = require('critical');
+
+  critical.generate({
+    inline: true,
+    base: '../../static/',
+    src: 'index.html',
+    dest: 'dist/index-critical.html',
+    width: 1400,
+    height: 800,
+    minify: true
+  });
+  critical.generate({
+    inline: true,
+    base: '../../static/',
+    src: 'websitelikethis.html',
+    dest: 'dist/index-wlt.html',
+    width: 1400,
+    height: 800,
+    minify: true
+  });
   done();
 });
